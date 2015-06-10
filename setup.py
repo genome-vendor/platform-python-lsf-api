e6ebf4f03553d8338e2f2350cd2e1d56f2094415
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-06-10 15:58:38 -0500

    Hack distutils so that extensions are built before python modules

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-# 
+#
 # (C) Copyright IBM Corporation 2013
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the Eclipse Public License.
-# 
+#
 import os
 from distutils.core import setup, Extension
+
+# http://sourceforge.net/mailarchive/message.php?msg_id=28474701
+# hack distutils so that extensions are built before python modules;
+# this is necessary for SWIG-generated .py files
+from distutils.command.build import build
+build.sub_commands = [('build_ext', build.has_ext_modules),
+                     ('build_py', build.has_pure_modules),
+                     ('build_clib', build.has_c_libraries),
+                     ('build_scripts', build.has_scripts)]
+
 setup(name='platform-python-lsf-api',
       version='1.0.1',
       description='Python binding for Platform LSF APIs',
@@ -19,7 +29,7 @@ setup(name='platform-python-lsf-api',
                                include_dirs=['/usr/include/python2.4'],
                                library_dirs=[os.environ['LSF_LIBDIR']],
                                swig_opts=['-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],
-                               extra_compile_args=['-m64', '-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],			
+                               extra_compile_args=['-m64', '-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],
                                extra_link_args=['-m64'],
                                libraries=['c', 'nsl', 'lsf', 'bat',
                                             'fairshareadjust', 'lsbstream'])],
